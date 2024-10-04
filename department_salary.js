f277556aff3e7684993e49e9cc74a24530b713a5
Laura Comar <lautmcomar@gmail.com>
2024-10-04 14:40:06 -0400

    Create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -51,3 +51,27 @@ const company = {
         }
     ]
 }; // used Sample data to create deparments structure 
+
+// Task 2: reate a Recursive Function to Calculate Total Salary for a Department
+function calculateDepartmentSalary(department) {
+    let totalSalary = 0; // iniciate total salary
+    department.employees.forEach(employee => { // loop through each employee (forEach)
+        totalSalary += employee.salary; // add employee salary to total
+        if (employee.subordinates.length > 0) { //if employee has subordinate add their salaries
+            employee.subordinates.forEach(subordinate => {
+                totalSalary += calculateEmployeeSalary(subordinate); // recursive calling function calculateEmployee that will be defined 
+            });
+        }
+    });
+    return totalSalary; // tot salary for department
+}
+
+function calculateEmployeeSalary(employee) {
+    let totalSalary = employee.salary; // current employee salary
+    if (employee.subordinates.length > 0) { // if employees subordinate has subordinates add their salaries
+        employee.subordinates.forEach(subordinate => {
+            totalSalary += calculateEmployeeSalary(subordinate); // function calls itself to find subordinates' own subordinate
+        });
+    }
+    return totalSalary; //tot salary for employee and subordinates
+}; 
